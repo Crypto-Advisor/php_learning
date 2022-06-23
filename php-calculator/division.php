cf84841ec6fd3e94a77185f460b28b2b7c278a12
Adrien <adriennavarre@hotmail.com>
2022-06-22 18:24:42 -0700

    php calculator

---
+++ php-calculator/division.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+
+<?= "Answer= " . ($_GET['num1'] / $_GET['num2']) . "\n" ?> <br>
+<a href="index.php">Reset</a>
+</body>
+</html>
---
+++ php-calculator/index.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<h3>Addition</h3>
+<form method='get' action="addition.php">
+  First Num:<input type='number' name='num1'/><br>
+  Second Num:<input type='number' name='num2'/><br>
+  <button type='submit'>Add</button>
+  <button type='submit' formaction="division.php">Divide</button>
+</form>
+
+<a href="index.php">Reset</a>
+</body>
+</html>
